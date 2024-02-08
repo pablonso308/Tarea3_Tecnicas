e3561d81a64ac80d9efc0ec863077d47f5251c58
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-08 09:53:54 +0100

    inicio ejercicio 1 desarrollo clase publicacion

---
+++ Ejercicio1/idea/Publicacion.java
@@ -0,0 +1,12 @@
+package Ejercicio1.idea;
+
+public class Publicacion {
+    private String texto;
+
+    public Publicacion(String texto) {
+        this.texto = texto;
+    }
+    public String getTexto() {
+        return texto;
+    }
+}
