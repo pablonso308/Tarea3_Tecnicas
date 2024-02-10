d5a4a071e7d20013c322e240346f43f96cee2ba1
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-10 12:34:49 +0100

    ejercicio4 definicion de jugador

--- Ejercicio4/jugador.java
+++ Ejercicio4/jugador.java
@@ -1,4 +1,11 @@
 package Ejercicio4;
 
+import java.util.Scanner;
+
 public class jugador {
+    Scanner sc = new Scanner(System.in);
+    public boolean resolverdesafio(desafio desafio) {
+        System.out.println("Resolviendo desafío: " + desafio.getdescripcion());
+        return true;
+    }
 }
