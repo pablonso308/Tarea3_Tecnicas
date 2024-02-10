7b6701136c67d314dcdca7fc854de08828d250e2
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-10 12:42:21 +0100

    ejercicio4 verdadera definicion de jugador

--- Ejercicio4/jugador.java
+++ Ejercicio4/jugador.java
@@ -3,9 +3,17 @@ package Ejercicio4;
 import java.util.Scanner;
 
 public class jugador {
+    private Scanner scanner;
     Scanner sc = new Scanner(System.in);
-    public boolean resolverdesafio(desafio desafio) {
-        System.out.println("Resolviendo desafío: " + desafio.getdescripcion());
-        return true;
+
+    public boolean resolverdesafio(desafio desafio) { System.out.println("Resolviendo desafío: " + desafio.getdescripcion());
+        System.out.print("¿El desafío se resuelve correctamente? (true/false): ");
+        return scanner.nextBoolean();
+    }
+
+    public desafio crearDesafio() {
+        System.out.print("Ingrese la descripción del nuevo desafío: ");
+        String descripcion = scanner.next();
+        return new desafio(descripcion);
     }
 }
