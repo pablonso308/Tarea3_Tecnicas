e0095827447369a81058073e6b777bb030ac6e89
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-02-10 12:20:03 +0100

    ejercicio4 definicion de desafio y main

--- Ejercicio4/Ejercicio4.java
+++ Ejercicio4/Ejercicio4.java
@@ -1,4 +1,21 @@
 package Ejercicio4;
 
+import java.util.ArrayList;
+
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        desafio loop = new desafio("Implementa un bucle");
+        desafio condicional = new desafio("Añade una estructura condicional");
+
+        ArrayList<desafio> desafiosNivel1 = new ArrayList<>();
+        desafiosNivel1.add(loop);
+        desafiosNivel1.add(condicional);
+
+    }
 }
+
+
+
+
+
+
--- Ejercicio4/desafio.java
+++ Ejercicio4/desafio.java
@@ -1,4 +1,11 @@
 package Ejercicio4;
 
 public class desafio {
+    private String descripcion;
+    public desafio(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getdescripcion() {
+        return descripcion;
+    }
 }
